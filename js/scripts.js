ba75144d718240b90a8817b840b93f4959c6173d
Joanna Strzelecka <joastr1@gamil.com>
2016-11-25 20:52:00 +0100

    add triangleArea script

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@
 var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+var a = 9;
+var h = 10;
+var triangleArea = a*h/2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+var a1 = prompt('Wpisz długość podstawy trójkąta');
+var h1 = prompt('Wpisz wysokość trójkąta');
+var triangleArea1 = a1*h1/2;
+alert('Pole trójkąta o podstawie a: ' + a1 + ' i wysokości h: ' + h1 + ' wynosi: ' + triangleArea1);
