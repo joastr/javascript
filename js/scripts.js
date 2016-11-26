f68fb07dd6d75a6f196f902849800ac7b0680e16
Joanna Strzelecka <joastr1@gamil.com>
2016-11-26 10:24:00 +0100

    if & else if & else - task

--- js/scripts.js
+++ js/scripts.js
@@ -26,8 +26,11 @@ if (value = 0) {
 var a = 5;
 var b = 6;
 var value = (a*a) + (2 * a * b) - (b*b);
+
 if (value > 0) {
 	console.log("wynik dodatni");
+} else if (value < 0) {
+	console.log("wynik ujemy");
 } else {
-	console.log("wynik ujemny lub zerowy");
+	console.log('wynik zerowy');
 }
