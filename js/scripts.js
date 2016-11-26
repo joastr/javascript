2b87b4dd4a1e3a87f2302dc812930b27c780c995
Joanna Strzelecka <joastr1@gamil.com>
2016-11-26 09:59:32 +0100

    first part task 8.5 kodilla

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,21 @@
 var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+//zadanie 8.5
+
+var a = 3
+var b = -3
+var value = (a*a) + (2 * a * b) - (b*b);
+if (value > 0) {
+	console.log('wynik dodatni')
+}
+
+if (value < 0) {
+	console.log ('wynik ujemny')
+}
+
+if (value = 0) {
+	console.log ('wynik zerowy')
+}
+
