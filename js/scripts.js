cf75782ed036f687008d7238b65d3b25f0fcc13f
Joanna Strzelecka <joastr1@gamil.com>
2016-11-28 10:10:54 +0100

    deleted additional code

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,5 @@
-// plik scripts.js
-/*
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
-*/
-//zadanie 8.5
 
-var a = 3
-var b = -3
-var value = (a*a) + (2 * a * b) - (b*b);
-if (value > 0) {
-	console.log('wynik dodatni')
-}
-
-if (value < 0) {
-	console.log ('wynik ujemny')
-}
-
-if (value = 0) {
-	console.log ('wynik zerowy')
-}
-
-// zadanie 8.5 druga część
+// zadanie 8.5 
 
 var a = 5;
 var b = 6;
